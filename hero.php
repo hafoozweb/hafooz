38220f659efb088a3df74f49139d2188744e0051
hafoozweb <156428386+hafoozweb@users.noreply.github.com>
2024-09-02 09:50:28 +0530

    Add files via upload

---
+++ hero.php
@@ -0,0 +1,68 @@
+<!-- Hero -->
+
+<style>
+.background-hrv1 {
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+}
+</style>
+<section class="hero hero-section">
+<div class="background-overlay">
+    <img class="background-hrv1" src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/banner.webp">
+    <video id="heroVideo" autoplay muted loop playsinline>
+        <source src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/hafooz%20hero%20section%20(1).mp4" type="video/mp4">
+    </video>
+</div>
+
+    <div class="outer">
+        <div class="outereffect" id="cog">
+            <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/outer1.webp">
+            <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/outer2.webp">
+        </div>
+    </div>
+    <div class="inner">
+        <div class="innereffect" id="innercog">
+            <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/inner.webp">
+        </div>
+    </div>
+
+    <div class="inner-hero d-flex justify-content-between align-items-center">
+        <div class="container text-center text-white">
+            <div class="row d-flex justify-content-center">
+                
+                <div class="circle">
+                    <div class="circlesimg" id="maincog">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/round-box.webp">
+                    </div>
+                    <div class="circle-item" id="lead-generation">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/Lead%20Generation.png" alt="Lead Generation">
+                        <p>Lead Generation</p>
+                    </div>
+                    <div class="circle-item" id="lead-nurturing">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/Lead%20Nurturing.png" alt="Lead Nurturing">
+                        <p>Lead Nurturing</p>
+                    </div>
+                    <div class="circle-item" id="quotation">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/Order%20Creation.png" alt="Order Creation">
+                        <p>Order Creation</p>
+                    </div>
+                    <div class="circle-item" id="order-creation">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/Quotation.png" alt="Quotation">
+                        <p>Quotation</p>
+                    </div>
+                    <div class="circle-item" id="customer-support">
+                        <img src="https://raw.githubusercontent.com/hafoozweb/hafooz/main/Customer%20Support.png" alt="Customer Support">
+                        <p>Customer Support</p>
+                    </div>
+                </div>
+
+                <div class="content animate">
+                    <h1 class="animate__animated animate__fadeInDown"><span class="gradient">AI-Driven</span><br> CRM Software for <br>Accelerated Sales Growth</h1>
+                    <button class="try-button"><a href="https://app.hafooz.com/company-signup">Try it for Free</a></button>
+                </div>
+
+            </div>
+        </div>
+    </div>    
+</section>
